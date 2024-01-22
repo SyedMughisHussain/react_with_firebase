20e7fb0b4d6f1b1da1aff261a21c3ced87960199
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-01-22 08:49:32 +0500

    hello

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,90 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { db } from "../config/firebase_config";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const todoText = useRef();
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    renderTodo();
+  }, []);
+
+  const renderTodo = async () => {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      todos.push(doc.data(), {docId: doc.id});
+      setTodos([...todos]);
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
   const addTodo = async (event) => {
     event.preventDefault();
     const todoValue = todoText.current.value;
-    todos.push({todo: todoValue});
     const docRef = await addDoc(collection(db, "todos"), {
       todo: todoValue,
     });
-    console.log("Document written with ID: ", docRef.id);
+    todos.push({ todo: todoValue, docId: docRef.id });
     setTodos([...todos]);
+    console.log("Document written with ID: ", docRef.id);
     todoText.current.value = "";
   };
 
+  const editTodo = (index) => {
+    const newValue = prompt("Enter the new value to enter.", todos[index].todo);
+    todos[index].todo = newValue;
+    setTodos([...todos]);
+  };
+
+  const deleteTodo = (index) => {
+    todos.splice(index, 1);
+    setTodos([...todos]);
+  };
   console.log(todos);
 
   return (
     <div className="w-screen h-screen flex justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <div className="bg-white h-fit w-fit p-5 mt-10">
+      <div className="bg-gray-200 h-fit w-fit p-5 mt-10">
         <p className="text-center font-bold text-3xl">Todo App</p>
         <form onSubmit={addTodo}>
           <input
-            className="border focus:outline-none mt-5 border-black p-2 w-[450px]"
+            className="border focus:outline-none mt-5 border-black p-3 w-[450px]"
             type="text"
             ref={todoText}
             placeholder="Add Todo"
           />
           <button className="ml-5 bg-purple-500 text-white p-2 ">Add</button>
         </form>
+        {todos.map((item, index) => {
+          return (
+            <div
+              className="flex bg-white justify-between p-2 mt-2 w-[450px]"
+              key={index}
+            >
+              <p className="text-[18px]">{item.todo}</p>
+              <div>
+                <button
+                  onClick={() => {
+                    editTodo(index);
+                  }}
+                  className="bg-green-600 p-1 text-white rounded-md"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => {
+                    deleteTodo(index);
+                  }}
+                  className="bg-red-500 ml-2 p-1 text-white rounded-md"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
